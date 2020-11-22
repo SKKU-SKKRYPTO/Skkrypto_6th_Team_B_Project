cca52aaf9c6a343e32ceae21fa8d9f13534b1ecc
bestowing <bestowing@naver.com>
2020-11-22 22:34:03 +0900

    Update: convert to  styled-component

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -4,19 +4,34 @@ import matching from "../routes/matching";
 import introduction from "../routes/introduction";
 import mypage from "../routes/mypage";
 import Navigation from "./Navigation";
-import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    /* width: 100vw;
+    height: 100vh; */
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className = "container">
-      <HashRouter>
-        <Navigation />
-        <Route path="/matching" exact={true} component={matching} />
-        <Route path="/introduction" exact={true} component={introduction} />
-        <Route path="/mypage" exact={true} component={mypage} />
-      </HashRouter>
-    </div>
+      <Container>
+        <HashRouter>
+          <Navigation />
+          <Route path="/matching" exact={true} component={matching} />
+          <Route path="/introduction" exact={true} component={introduction} />
+          <Route path="/mypage" exact={true} component={mypage} />
+        </HashRouter>
+      </Container>
     );
   }
 }
--- front-end/src/components/Navigation.js
+++ front-end/src/components/Navigation.js
@@ -1,25 +1,78 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import "./Navigation.css";
 import logo from "../images/logo.png";
+import styled from "styled-components";
 
+const Nav = styled.div`
+	display: flex;
+	/* position: absolute; */
+	width: 100%;
+	height: 200px;
+`;
+
+const Logo = styled.div`
+	margin: 0px;
+	padding-left: 50px;
+`;
+
+const Menu = styled.div`
+	justify-content: center;
+	width: 100%;
+	left: 0px;
+	top: 0px;
+	padding-top: 74px;
+	padding-bottom: 76px;
+`;
+
+const List = styled.ul`
+	list-style: none;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	
+	padding: 0px 30px;
+	margin: 0px;
+`;
+
+const Item = styled.li`
+	padding: 0px 30px;
+	/* font-family: LG Smart UI; */
+	font-style: normal;
+	font-weight: bold;
+	font-size: 36px;
+	line-height: 20px;
+`;
+
+const MenuLink = styled(Link)`
+	color: #000000;
+	text-decoration: none;
+	&:hover {
+		border-bottom: 3px solid black;
+    padding-bottom: 5px;
+	}
+`;
 
 function Navigation() {
-    return (
-        <div className="nav">
-            <div className="logo">
-                <img src ={logo} width="300" height="300"/>
-            </div>
-            <div className="menu">
-                <ul>
-                    <li><Link to="/matching" >매칭 서비스</Link></li>
-                    <li><Link to="/introduction" >서비스 소개</Link></li>
-                    <li><Link to="/mypage" >마이페이지</Link></li>
-                </ul>
-            </div>
-            
-        </div>
-    );
+	return (
+		<Nav>
+			<Logo>
+				<img src ={logo} width="300" height="300"/>
+			</Logo>
+			<Menu>
+				<List>
+					<Item>
+						<MenuLink to="/matching" >매칭 서비스</MenuLink>
+					</Item>
+					<Item>
+						<MenuLink to="/introduction" >서비스 소개</MenuLink>
+					</Item>
+					<Item>
+						<MenuLink to="/mypage" >마이페이지</MenuLink>
+					</Item>
+				</List>
+			</Menu>
+		</Nav>
+	);
 }
 
 export default Navigation;
