5499c181a0ef9211eec4c87371178a4247e82647
hyunjeongshub <71911721+hyunjeongshub@users.noreply.github.com>
2020-11-28 13:42:01 +0900

    Update ShareContract.sol

--- contract/ShareContract.sol
+++ contract/ShareContract.sol
@@ -13,16 +13,24 @@ contract ShareContract {
     
     mapping (address => uint) getPartyIdx;  // 사용자의 파티 인덱스
     mapping (address => bool) isRefunded;   // 사용자가 환불을 받은적이 있는지 체크함
-
+    
     uint emptyPartyIdx = 1;                 // 비어있는 파티 인덱스
     bool isEmptyParty = false;              // 빈 파티 유무
     uint totalPartyNumber = 0;                   // 방 개수
-    uint value = 1000000000000000000;       // 1Klay, peb단위
+    uint value = 100000000000000000;        // 0.1Klay, peb단위
+    uint voteCount = 0;                     //투표한 사람 수
+    uint cons = 0;                          //방 폭파 반대표 개수
+    uint startVote = 0;                     //투표 시작 시간
+    bool canRefund = false;                 //환불 가능
 
     struct Party {
         address owner;
         uint startTime;                     // 방장이 start한 시점의 시간 저장
         uint people;
+    }
+    
+    struct Vote {
+        address owner;
     }
     
     /*
@@ -77,7 +85,7 @@ contract ShareContract {
     **          더 이상 비어있는 방이 없을 경우, isEmptyParty 변수를 false로 함.
     */
     function _updateEmptyPartyIdx() internal {
-        emptyPartyIdx++;
+        emptyPartyIdx++;      
         if (emptyPartyIdx >= totalPartyNumber + 1)
             isEmptyParty = false;
     }
@@ -87,11 +95,11 @@ contract ShareContract {
     ** 인자:    x
     ** 반환값:  x
     ** 설명:    파티장이 파티를 시작하는 함수.
-    **          한 달간 계약이 진행되고 (파티장이 입력한 공유 계정 정보가 전달됨),
+    **          계약이 진행되고 (파티장이 입력한 공유 계정 정보가 전달됨),
     **          이 순간부터 계약을 위반하는 구성원 처벌이 가능.
     */
     function startParty() public {
-        uint idx = getPartyIdx[msg.sender];
+        uint idx = getPartyIdx[msg.sender]; 
         require(parties[idx].owner == msg.sender); // 방장이 이 함수를 호출해야함
         require(parties[idx].people == 4); // 4명일때만 시작 가능
         parties[idx].startTime = now;
@@ -102,6 +110,7 @@ contract ShareContract {
     ** 조건:    1. 계약 시작 이후 30일이 경과하지 않아야 함.
     **          2. 이전에 환불을 받은 적이 없어야 함.
     **          3. 파티장이 아니어야 함.
+    **          4. 투표 결과 환불 대상자여야 함.
     ** 설명:    계약 위반으로 파티가 조기 종료되는 경우,
     **          환불을 받을 조건을 명시.
     */
@@ -110,6 +119,7 @@ contract ShareContract {
         uint normalDays = now - parties[senderPartyIdx].startTime;
         require(normalDays < 30 days);
         require(isRefunded[msg.sender] == false);
+        require(canRefund == true);
         require(msg.sender != parties[senderPartyIdx].owner);
         _;
     }
@@ -123,25 +133,10 @@ contract ShareContract {
     **          지불한 금액에서 파티가 정상적으로 진행된 날짜만큼 차감하여 환불함.
     */
     function _calculateRefund(uint startTime) internal view returns (uint) {
-        uint passedDay = now - startTime;
-        uint oneDayCost = value / 30; // 하루당 가격
-        uint refund = oneDayCost * (30 days - passedDay); // 하루당가격 * 남은 기간 (환불받을 가격)
+        uint passedDay = now - startTime;//now,startTime은  초단위
+        uint oneDayCost = value / (30*24*60*60); // 1초당 가격 
+        uint refund = oneDayCost * (30 days - passedDay); // 1초당가격 * 남은 기간(초) (환불받을 가격)
         return refund;
-    }
-    
-    /*
-    ** kickSomeone() 함수
-    ** 인자:    address _criminal  (계약 위반자)
-    ** 반환값:  x
-    ** 설명:    계약 위반으로 파티가 조기 종료되는 경우,
-    **          계약 위반자를 제외한 파티원에게 환불이 이루어지는 함수.
-    */
-    function kickSomeone(address _criminal) external refundable() {
-        getPartyIdx[msg.sender] = 0;
-        require(msg.sender != _criminal);
-        uint partyIdx = getPartyIdx[msg.sender];
-        isRefunded[msg.sender] = true;
-        transfer(_calculateRefund(parties[partyIdx].startTime));
     }
 
     /*
@@ -157,7 +152,7 @@ contract ShareContract {
         getPartyIdx[msg.sender] = 0;
         transfer(_calculateRefund(parties[partyIdx].startTime));
     }
-    
+ 
     /*
     ** withdrawBreakParty() 함수
     ** 인자:    x
@@ -167,10 +162,11 @@ contract ShareContract {
     **       여기서 now는 조기 종료된 시점이어야하므로 조기 종료되었을 때 해당 함수를 바로 실행해야함
     */
     function withdrawBreakParty() public {
+        require(canRefund == true);
         uint partyIdx = getPartyIdx[msg.sender];
         uint passedDay = now - parties[partyIdx].startTime;
-        uint oneDayCost = value / 30; // 하루당 가격
-        uint refund = oneDayCost * passedDay; // 하루당가격 * 지난 기간
+        uint oneDayCost = value / (30*24*60*60); // 1초당 가격
+        uint refund = oneDayCost * passedDay; // 1초당가격 * 지난기간
 
         getPartyIdx[msg.sender] = 0;
         transfer(refund);
@@ -225,4 +221,52 @@ contract ShareContract {
         msg.sender.transfer(_value);
         return true;
     }
-}
+    
+    /* createVote() 함수
+    ** 인자 :   x
+    ** 반환값 : x
+    ** 설명 :   누군가 이 함수를 실행하면(투표 제기) 나머지 사람들에게 투표하라는 알림보냄.
+    **          변수 값을 초기화.
+    */
+    function createVote() public {
+        voteCount = 0;                     //투표한 사람 수
+        cons = 0;                          //방 폭파 찬성표 개수
+        startVote = now;                   //투표 시작 시간
+        canRefund = false;
+    }
+    
+    /*
+    ** vote() 함수
+    ** 인자 :   bool(방 폭파 찬성 1, 반대 0)
+    ** 반환값:  x  
+    ** 설명 :   방 폭파 반대 수와 총 투표 수를 기록. 4명 다 투표를 진행하면, voteResult 실행.
+    **          만약 투표자가 4명이 안될경우 3일 지나면 자동 voteResult 실행.
+    */
+    function voting(bool election) public {
+        //createVote가 실행될 때만 가능한 require 고민해보기
+        //한명이 여러번 투표 방지 고민해보기
+        if(election == false){
+            cons++;
+        }
+        voteCount++;
+        if(voteCount == 3){
+            voteResult();
+        }
+        uint votingTime;
+        votingTime = now - startVote;
+        if(votingTime >= 3 days){
+            voteResult();
+        }
+    }
+    
+    /*
+    ** voteResult() 함수
+    ** 인자 :   x
+    ** 반환값:  x  
+    ** 설명 :   투표 결과, 반대표가 없으면 환불 가능.
+    */
+    function voteResult() private{
+        if(cons != 0){
+            canRefund = true;
+        }
+    }
